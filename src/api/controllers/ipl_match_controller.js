9c86a8defdcb89e0a53a2c220c88446712811fe3
dipali pawshe <dipali.pawshe@in10media.com>
2021-09-28 16:28:53 +0530

    fixturesdata

--- src/api/controllers/ipl_match_controller.js
+++ src/api/controllers/ipl_match_controller.js
@@ -253,7 +253,7 @@ module.exports = class MatchController {
         var filters = req.body;
         filters.matchState = ["U"];
         filters.team = req.body.team ? [req.body.team] : ["m", "w"]
-        filters.team_id = req.body.team_id ? req.body.team_id:"",
+        //filters.team_id = req.body.team_id ? req.body.team_id:"",
         filters.startDate = new Date("2008-01-01").toISOString();
         filters.endDate = new Date("2021-01-01").toISOString();
         console.log("In apis list: ", filters);
