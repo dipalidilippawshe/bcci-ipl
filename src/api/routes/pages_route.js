0ddc75f2b1541456cb1cc2f35656bad05a9c464e
dipali pawshe <dipali.pawshe@in10media.com>
2021-10-07 11:54:38 +0530

    pages-controller news landing page

--- src/api/routes/pages_route.js
+++ src/api/routes/pages_route.js
@@ -8,5 +8,7 @@ router.get("/app/home", pagesCtrl.appHomepage);
 router.get("/web/home", pagesCtrl.webHomepage);
 router.get("/app/video-list", pagesCtrl.appVideoLIstpage);
 router.get("/web/video-list", pagesCtrl.webVideoLIstpage);
+router.get("/app/news-list", pagesCtrl.appNewsLIstpage);
+router.get("/web/news-list", pagesCtrl.webNewsLIstpage);
 
 module.exports = router;
