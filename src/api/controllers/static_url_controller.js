2077eb34b8606142e640c77089813f690ac4af48
dipali pawshe <dipali.pawshe@in10media.com>
2021-10-20 12:52:12 +0530

    status

--- src/api/controllers/static_url_controller.js
+++ src/api/controllers/static_url_controller.js
@@ -1,6 +1,10 @@
 const configfile = require("../../../config/default.json")
 var crypto = require("crypto");
 var fs = require("fs");
+
+var request = require('request');
+const client_id = "ks00764008@techmahindra.com";
+const client_secret = "techmtest";
 module.exports = class ConfigController {
     
   static async getConfigdata(req,res,next)
@@ -49,10 +53,23 @@ module.exports = class ConfigController {
   }
   
   static async publicKeyRegistration(req,res,next){
-
-    const client_id = "ks00764008@techmahindra.com";
-    const client_secret = "techmtest";
-
+    console.log("client_id: ",client_id);
+    var auth_string = new Buffer(client_id + ":" + client_secret).toString('base64');
+    console.log(auth_string);
+    request({
+    method: 'POST',
+    url: 'https://oauth.brightcove.com/v4/access_token',
+    headers: {
+    'Authorization': 'Basic ' + auth_string,
+    'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: 'grant_type=client_credentials'
+    }, function (error, response, body) {
+    console.log('Status: ', response.statusCode);
+    console.log('Headers: ', JSON.stringify(response.headers));
+    console.log('Response: ', body);
+    console.log('Error: ', error);
     
-  }
+  })
+ }
 }
